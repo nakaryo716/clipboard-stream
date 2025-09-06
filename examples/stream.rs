ba0fa8ca2917d66f9a499bf1591b7f55ce639ba7
Ryo Nakaya <nakayaryo0716@gmail.com>
2025-09-06 19:55:53 +0900

    refactor: format code

--- examples/stream.rs
+++ examples/stream.rs
@@ -1,14 +1,14 @@
 use clipboard_stream::event_listener::ClipboardEventListener;
 use futures::StreamExt;
 
-
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let event=  ClipboardEventListener::new();
-    let mut s = event.new_stream(clipboard_stream::body::Kind::Utf8).unwrap();
+    let event = ClipboardEventListener::new();
+    let mut s = event
+        .new_stream(clipboard_stream::body::Kind::Utf8)
+        .unwrap();
 
     while let Some(v) = s.next().await {
         println!("{:?}", v);
     }
-
 }
