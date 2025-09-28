e308730a588041173500525e513a19f87eb41304
Ryo Nakaya <128471892+nakaryo716@users.noreply.github.com>
2025-09-28 12:02:38 +0900

    refactor: separating thread spawning and observing responsibilities within the driver (#12)
    To separate role spawing a thread and observing clipboard change event, intoroduce Observer trait and implemented for each OS.
    Driver construct a struct that is implemented Observer trait and call Observer::observe method in a OS thread.

---
+++ src/driver/observer.rs
@@ -0,0 +1,71 @@
+use std::{
+    sync::{
+        Arc, Mutex,
+        atomic::{AtomicBool, Ordering},
+    },
+    time,
+};
+
+use crate::{
+    Kind,
+    body::{Body, BodySenders},
+    sys::OSXSys,
+};
+
+/// A trait observing clipboard change event and send data to receiver([`ClipboardStream`])
+pub(super) trait Observer {
+    fn observe(&self, body_senders: Arc<Mutex<BodySenders>>);
+}
+
+/// Observer for MacOS
+pub(super) struct OSXObserver {
+    stop: Arc<AtomicBool>,
+}
+
+impl OSXObserver {
+    pub(super) fn new(stop: Arc<AtomicBool>) -> Self {
+        OSXObserver { stop }
+    }
+}
+
+impl Observer for OSXObserver {
+    fn observe(&self, body_senders: Arc<Mutex<BodySenders>>) {
+        let mut last_count = None;
+
+        let mut sys = OSXSys;
+        while !self.stop.load(Ordering::Relaxed) {
+            std::thread::sleep(time::Duration::from_millis(200));
+            let change_count = match sys.get_change_count() {
+                Ok(c) => c,
+                Err(_) => {
+                    let mut gurad = body_senders.lock().unwrap();
+                    gurad.send_all_if_some(Err(crate::error::Error::GetItem));
+                    continue;
+                }
+            };
+
+            if Some(change_count) == last_count {
+                continue;
+            }
+            last_count = Some(change_count);
+
+            match sys.get_item() {
+                Ok(item) => {
+                    let mut gurad = body_senders.lock().unwrap();
+                    let body = Ok(Body::Utf8String(item));
+                    if let Err(e) = gurad.try_send_if_some(body, &Kind::Utf8String) {
+                        eprintln!("{}", e);
+                    }
+                }
+                Err(_) => {
+                    let mut gurad = body_senders.lock().unwrap();
+                    if let Err(e) =
+                        gurad.try_send_if_some(Err(crate::error::Error::GetItem), &Kind::Utf8String)
+                    {
+                        eprintln!("{}", e);
+                    }
+                }
+            }
+        }
+    }
+}
