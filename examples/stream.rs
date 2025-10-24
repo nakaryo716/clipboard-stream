b93e4b8aef9fefe49522872b1aff91bbde775cb6
Ryo Nakaya <nakayaryo0716@gmail.com>
2025-10-24 20:59:09 +0900

    reflect library changes to handle Body::PNG variant

--- examples/stream.rs
+++ examples/stream.rs
@@ -1,6 +1,8 @@
 use clipboard_stream::{Body, ClipboardEventListener};
 use futures::StreamExt;
 
+use std::{fs::File, io::Write};
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let mut event_listener = ClipboardEventListener::spawn();
@@ -10,6 +12,14 @@ async fn main() {
         match content {
             Body::Utf8String(text) => {
                 println!("got string: {}", text);
+            }
+            #[cfg(target_os = "macos")]
+            Body::PNG(v) => {
+                println!("got PNG");
+                tokio::task::spawn_blocking(move || {
+                    let mut file = File::create("clip-img.png").unwrap();
+                    file.write_all(v.as_ref()).unwrap();
+                });
             }
         }
     }
