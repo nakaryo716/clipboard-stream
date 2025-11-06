f2229143452c1659527de585f033f59c4ef7ff3d
Ryo Nakaya <nakayaryo0716@gmail.com>
2025-11-06 20:54:24 +0900

    docs: reflect library changes to example code

--- examples/stream.rs
+++ examples/stream.rs
@@ -1,9 +1,13 @@
-use clipboard_stream::{Body, MimeType, ClipboardEventListener};
+use clipboard_stream::{Body, ClipboardEventListener, MimeType};
 use futures::StreamExt;
+use tokio::{fs::File, io::AsyncWriteExt};
 
-use std::{fs::File, io::Write, sync::{atomic::{AtomicU32, Ordering}, Arc}};
+use std::sync::{
+    Arc,
+    atomic::{AtomicU32, Ordering},
+};
 
-#[tokio::main(flavor = "current_thread")]
+#[tokio::main]
 async fn main() {
     let mut event_listener = ClipboardEventListener::spawn();
     let mut stream = event_listener.new_stream(32);
@@ -11,21 +15,25 @@ async fn main() {
 
     while let Some(content) = stream.next().await {
         match content {
+            // print text when text is copied
             Body::Utf8String(text) => {
                 println!("got string: {}", text);
             }
+            // create png image file when png image data is copied
             Body::Image {
                 mime: mime_type,
                 data: v,
             } => {
                 println!("got: {:?}", mime_type);
                 let cc = count.clone();
-                tokio::task::spawn_blocking(move || {
+                // create png image file
+                tokio::task::spawn(async move {
                     if mime_type == MimeType::ImagePng {
-                        let num = cc.fetch_add(1, Ordering::Relaxed);
+                        let num = cc.fetch_add(1, Ordering::SeqCst);
                         let file_name = format!("clip-img-{}.png", num);
-                        let mut file = File::create(file_name).unwrap();
-                        file.write_all(v.as_ref()).unwrap();
+
+                        let mut file = File::create(file_name).await.unwrap();
+                        file.write_all(v.as_ref()).await.unwrap();
                     }
                 });
             }
