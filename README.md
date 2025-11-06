87fc765c57fa0032d023da53b81b3167736cebf4
Ryo Nakaya <128471892+nakaryo716@users.noreply.github.com>
2025-11-06 21:05:58 +0900

    docs: refrect library changes to example code in library docs (#33)

--- README.md
+++ README.md
@@ -11,26 +11,53 @@ The main part of this crate is ClipboardStream. This struct implements Stream Tr
 ## Example
 The following example shows how to receive clipboard items:
 ```rust
-use clipboard_stream::{Body, ClipboardEventListener};
+use clipboard_stream::{Body, ClipboardEventListener, MimeType};
 use futures::StreamExt;
+use tokio::{fs::File, io::AsyncWriteExt};
+
+use std::sync::{
+    Arc,
+    atomic::{AtomicU32, Ordering},
+};
 
 #[tokio::main]
 async fn main() {
-    // Spawn a clipboard event listener
     let mut event_listener = ClipboardEventListener::spawn();
-    // Create a ClipboardStream with buffer
     let mut stream = event_listener.new_stream(32);
+    let count = Arc::new(AtomicU32::new(0));
 
-    // Text is printed when the clipboard is updated(Copy operation)
     while let Some(content) = stream.next().await {
         match content {
+            // print text when text is copied
             Body::Utf8String(text) => {
                 println!("got string: {}", text);
+            }
+            // create png image file when png image data is copied
+            Body::Image {
+                mime: mime_type,
+                data: v,
+            } => {
+                println!("got: {:?}", mime_type);
+                let cc = count.clone();
+                tokio::task::spawn(async move {
+                    if mime_type == MimeType::ImagePng {
+                        let num = cc.fetch_add(1, Ordering::SeqCst);
+                        let file_name = format!("clip-img-{}.png", num);
+
+                        let mut file = File::create(file_name).await.unwrap();
+                        file.write_all(v.as_ref()).await.unwrap();
+                    }
+                });
             }
         }
     }
 }
 ```
+
+# Body types
+In this library, we can handle thses body types.  
+- text
+- image
 
 ## Runtime
 Internally, this crate spawns a small dedicated OS thread to listen for clipboard events.
--- src/lib.rs
+++ src/lib.rs
@@ -9,24 +9,50 @@
 //! The following example shows how to receive clipboard items:
 //!
 //! ```no_run
-//! use clipboard_stream::{ClipboardEventListener, Body};
-//! use futures::stream::StreamExt;
+//! use clipboard_stream::{Body, MimeType, ClipboardEventListener};
+//! use futures::StreamExt;
+//! use tokio::{io::AsyncWriteExt, fs::File};
+//!
+//! use std::sync::{atomic::{AtomicU32, Ordering}, Arc};
 //!
 //! #[tokio::main]
 //! async fn main() {
-//!     // Spawn a clipboard event listener
 //!     let mut event_listener = ClipboardEventListener::spawn();
-//!
-//!     // Create a new stream
 //!     let mut stream = event_listener.new_stream(32);
+//!     let count = Arc::new(AtomicU32::new(0));
 //!
-//!     while let Some(body) = stream.next().await {
-//!         if let Body::Utf8String(text) = body {
-//!             println!("{}", text);
+//!     while let Some(content) = stream.next().await {
+//!         match content {
+//!             // print text when text is copied
+//!             Body::Utf8String(text) => {
+//!             println!("got string: {}", text);
+//!         }
+//!             // create png image file when png image data is copied
+//!             Body::Image {
+//!                 mime: mime_type,
+//!                 data: v,
+//!         } => {
+//!             println!("got: {:?}", mime_type);
+//!             let cc = count.clone();
+//!             tokio::task::spawn(async move {
+//!                 if mime_type == MimeType::ImagePng {
+//!                     let num = cc.fetch_add(1, Ordering::SeqCst);
+//!                     let file_name = format!("clip-img-{}.png", num);
+//!
+//!                     let mut file = File::create(file_name).await.unwrap();
+//!                     file.write_all(v.as_ref()).await.unwrap();
+//!                 }
+//!             });
+//!         }
 //!         }
 //!     }
 //! }
 //! ```
+//!
+//! # Body types
+//! In this library, we can handle thses body types.
+//! - text
+//! - image
 //!
 //! # Runtime
 //! Internally, this crate spawns a small dedicated OS thread to listen for clipboard events.
